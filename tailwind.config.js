11e9403a187a78c0070e762f70a94996f6844990
Jess Corda <jcor@camelot-itlab.com>
2021-12-29 16:27:30 +0530

    Create a NFT card preview component

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        colors: {
+            'soft_blue': 'hsl(215, 51%, 70%)',
+            'cyan': 'hsl(178, 100%, 50%)',
+            'white': 'hsl(0, 0%, 100%)',            
+            'main_bg': 'hsl(217, 54%, 11%)',
+            'card_bg': 'hsl(216, 50%, 16%)',
+            'line': 'hsl(215, 32%, 27%)'
+        },
+        fontFamily: {
+            'outfit': ["'Outfit'", 'sans-serif']
+        },
+    },
+    variants: {
+        extend: {},
+    },
+
+}
